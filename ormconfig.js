341490b1c8119d25e6911b7da600ae9bca653c2a
khaldgh <85020729+khaldgh@users.noreply.github.com>
2021-12-11 10:29:13 +0300

    new commit

--- ormconfig.js
+++ ormconfig.js
@@ -30,7 +30,7 @@ switch (process.env.NODE_ENV) {
         case 'production':
             Object.assign(dbConfig, {
                 type: 'mysql',
-                url: process.env.CLEARDB_DATABASE_URL,
+                url: 'mysql://b68cf19135f010:de0a0693@us-cdbr-east-04.cleardb.com/heroku_11b09383cddc9ee?reconnect=true',
                 migrationsRun: true,
                 entities: ['**.*.js'],
                 ssl: {
