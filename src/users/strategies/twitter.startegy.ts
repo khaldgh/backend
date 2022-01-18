e06a375cff4f84d05e19b8bdd01dbd9679d7b181
khaldgh <85020729+khaldgh@users.noreply.github.com>
2022-01-18 18:25:06 +0300

    d

---
+++ src/users/strategies/twitter.startegy.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Profile, Strategy } from "passport-twitter";
+
+@Injectable()
+export class TwitterStrategy extends PassportStrategy(Strategy, "twitter") {
+  constructor() {
+    // zPhYhU8AccxlQU14fqu8BuAk1 //API KEY
+    // J1bRMpPbNizIAHJ1JnAPeDFDgyXxB6JlLfljKBEvy45rug0gTA //API KEY SECRET
+    // AAAAAAAAAAAAAAAAAAAAACwCYQEAAAAAVIPW1uAA91QzT%2FPz5cKo8NiQvwc%3DYdZlgtoUj7rh5Uco4NuC6a3UHzC9gco7LbKnzwe9Rmq79CIW4t //BEARER TOKEN
+    super({
+      clientID: 'NGhjeExvMzViSExZQlB3SUhqZXQ6MTpjaQ',
+      clientSecret: 'VhsxafrakKoUg3bKucpHXwKFgVOeQu8IoswPddjryoPmKUzRgl',
+      callbackURL: "http://daleel-app.herokuapp.com/users/twitter/callback",
+      scope: "email",
+      profileFields: ["emails", "name"],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,12 +10,13 @@ import { GoogleStrategy } from 'src/users/strategies/google.strategy';
 import { FacebookStrategy } from 'src/users/strategies/facebook.strategy';
 import { InstagramAuthModule } from '@nestjs-hybrid-auth/instagram';
 import { HybridAuthModule } from '@nestjs-hybrid-auth/all';
+import { TwitterStrategy } from './strategies/twitter.startegy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, GoogleStrategy, FacebookStrategy],
+  providers: [UsersService, AuthService, GoogleStrategy, FacebookStrategy, TwitterStrategy],
 })
 export class UsersModule {
   configure(consumer: MiddlewareConsumer) {
