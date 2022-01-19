4fe1e5377df8f81406c9b0043e0d3f59cce92bac
khaldgh <85020729+khaldgh@users.noreply.github.com>
2022-01-19 21:48:59 +0300

    uncommented restaurant from GetRestaurantDto

--- src/places/dtos/get-restaurant.dto.ts
+++ src/places/dtos/get-restaurant.dto.ts
@@ -2,8 +2,8 @@ import { IsString } from 'class-validator';
 
 export class GetRestaurantDto {
 
-    // @IsString()
-    // resaurant: string
+    @IsString()
+    restaurant: string
 
     @IsString()
     city: string
